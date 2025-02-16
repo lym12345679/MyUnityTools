d65d4ab91e96bc8805fbd1cb2ba3870b4cbf0bbd
lym12345679 <1457545136@qq.com>
2025-02-16 23:56:20 +0800

    fit：更改需要发布的package的位置

--- Assets/Scripts/MizukiTool/Runtime/UITool/UIBox/BoxDict.cs
+++ Assets/Scripts/MizukiTool/Runtime/UITool/UIBox/BoxDict.cs
@@ -13,11 +13,12 @@ namespace MizukiTool.Box
         //用字典存储所有的UIEnum和类型
         public static Dictionary<System.Type, BoxEnum> boxTypeDic = new Dictionary<System.Type, BoxEnum>
         {
-            {typeof(MessageBox),BoxEnum.MessageBox}
+
         };
         //用字典存储所有的UI预制体路径
-        public static Dictionary<BoxEnum, string> BoxPathDic = new Dictionary<BoxEnum, string>{
-            { BoxEnum.MessageBox, "UIPrefab/MessageBox" }
+        public static Dictionary<BoxEnum, string> BoxPathDic = new Dictionary<BoxEnum, string>
+        {
+
         };
     }
 }
